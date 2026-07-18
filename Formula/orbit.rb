ac7d9b0302ee7322ceb44407c7713deb4034f7e9
linus <z.jh@me.com>
2026-07-18 14:54:38 +0000

    fix: fill real SHA256 hashes in orbit.rb formula

--- Formula/orbit.rb
+++ Formula/orbit.rb
@@ -7,18 +7,18 @@ class Orbit < Formula
   on_macos do
     on_arm do
       url "https://github.com/linuszz/orbt/releases/download/v#{version}/orbt-macos-aarch64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "895a161b70b419a928ef18127ce5f65b7719c7bb5d91cadf7fcf4886430ac755"
     end
     on_intel do
       url "https://github.com/linuszz/orbt/releases/download/v#{version}/orbt-macos-x86_64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "75c6bd87764f90c0347f17f100ed844f2e93f8e90aeab837bb89fee2b981f1af"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/linuszz/orbt/releases/download/v#{version}/orbt-linux-x86_64.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "44ab18ccbc82f46963bf7189b29aa0702311e5fd9ea22b1b31758cdfc990b38d"
     end
   end
 
